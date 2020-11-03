5efae374a0d0fb7ee6601103d700f064efd53137
lukdru <ldrukteinis@gmail.com>
2020-11-04 01:32:06 +0200

    Fixed routing / Bootstrap integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './main-page/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
