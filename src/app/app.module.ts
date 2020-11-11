94857acd27c2a6bd5a222c4c5ba68cc630cd5a5e
lukdru <ldrukteinis@gmail.com>
2020-11-11 05:56:53 +0200

    Added ending page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './main-page/header/header.component';
 import { PuzzleGameComponent } from './main-page/puzzle-game/puzzle-game.component';
+import { EndingPageComponent } from './ending-page/ending-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     HeaderComponent,
-    PuzzleGameComponent
+    PuzzleGameComponent,
+    EndingPageComponent
   ],
   imports: [
     BrowserModule,
